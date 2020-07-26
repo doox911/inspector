87d8be40d4960c8b15592f09d50a47bf3ac4c109
Polyakov Andrey <polyakov.a@stis.ru>
2020-07-26 23:41:09 +0300

    Добавлен тест на isNumber

---
+++ src/index.test.js
@@ -0,0 +1,77 @@
+import Inspector from './index';
+
+/**
+ * Parent Exception
+ */
+class DefaultException extends Error {
+  /**
+   * @param {string} message
+   */
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+/**
+ * @description Test exception
+ */
+class MyCustomException extends DefaultException {
+  /**
+   * @param {string} message
+   */
+  constructor(message) {
+    super(message);
+  }
+}
+
+/**
+ * @description for testing
+ */
+function testFunction() {
+
+}
+
+test('Test isNumber', () => {
+  expect(Inspector.isNumber(1, 'My message', MyCustomException)).toBe(Inspector);
+  try {
+    Inspector.isNumber('', 'My message', MyCustomException);
+  } catch (e) {
+    expect(e).toBeInstanceOf(MyCustomException);
+  }
+  try {
+    Inspector.isNumber(false, 'My message', MyCustomException);
+  } catch (e) {
+    expect(e).toBeInstanceOf(MyCustomException);
+  }
+  try {
+    Inspector.isNumber(undefined, 'My message', MyCustomException);
+  } catch (e) {
+    expect(e).toBeInstanceOf(MyCustomException);
+  }
+  try {
+    Inspector.isNumber(null, 'My message', MyCustomException);
+  } catch (e) {
+    expect(e).toBeInstanceOf(MyCustomException);
+  }
+  try {
+    Inspector.isNumber([], 'My message', MyCustomException);
+  } catch (e) {
+    expect(e).toBeInstanceOf(MyCustomException);
+  }
+  try {
+    Inspector.isNumber({}, 'My message', MyCustomException);
+  } catch (e) {
+    expect(e).toBeInstanceOf(MyCustomException);
+  }
+  try {
+    Inspector.isNumber(testFunction, 'My message', MyCustomException);
+  } catch (e) {
+    expect(e).toBeInstanceOf(MyCustomException);
+  }
+  try {
+    Inspector.isNumber(BigInt(11), 'My message', MyCustomException);
+  } catch (e) {
+    expect(e).toBeInstanceOf(MyCustomException);
+  }
+});
