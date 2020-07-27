efa19806b1d746d47bc6693fa63b9b93617a2c6e
Polyakov Andrey <polyakov.a@stis.ru>
2020-07-27 13:00:22 +0300

    Завершено написание версии 1.0.0

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
-import typeIs from './helpers/typeIs';
-
-/**
- * @description Checks that the type of the argument
- * and the expected type are equal.
- * If there is no match, throws a custom exception.
- * @param {*} variable
- * @param {string} type
- * @param {string} message
- * @param {Class} Exception
- * @return {Inspector}
- * @private
- */
-const _throwIfInvalid = (variable, type, message, Exception) => {
-  if (typeIs(variable) !== type) {
-    throw new Exception(message);
-  }
-
-  return Inspector;
-};
+const typeIs = require('./helpers/typeIs');
 
 /**
  * @description Class of checking and throwing a custom exception.
@@ -105,6 +86,25 @@ class Inspector {
     return _throwIfInvalid(variable, 'Symbol', message, Exception);
   }
 }
+
+/**
+ * @description Checks that the type of the argument
+ * and the expected type are equal.
+ * If there is no match, throws a custom exception.
+ * @param {*} variable
+ * @param {string} type
+ * @param {string} message
+ * @param {Class} Exception
+ * @return {Inspector}
+ * @private
+ */
+const _throwIfInvalid = (variable, type, message, Exception) => {
+  if (typeIs(variable) !== type) {
+    throw new Exception(message);
+  }
+
+  return Inspector;
+};
 
 module.exports = Inspector;
 
